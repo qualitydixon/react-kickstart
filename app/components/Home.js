5f3b678dc9cd19e463dc7dfe6e681affe1b389ec
Mike Dixon <qualitydixon@gmail.com>
2016-12-18 19:22:07 -0700

    Update packages. Add yarn.lock.

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
-  render () {
-    return (
+	render () {
+		return (
       <div>
         {'This is your home component, which is currently set as the IndexRoute in app/config/routes.js. It shows when no other routes are active.'}<br/>
         {'This boilerplate uses React, React-Router, Babel, ESLint, Webpack, and LESS'}
       </div>
     )
-  }
+	}
 }
