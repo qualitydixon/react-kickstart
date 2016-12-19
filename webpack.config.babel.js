683eec7f21ae98588bd5dbb009775c330634176e
Mike Dixon <qualitydixon@gmail.com>
2016-12-18 22:26:26 -0700

    webpack config changes

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,7 @@
 import webpack from 'webpack'
 import path from 'path'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 	template: path.join(__dirname, '/app/index.html'),
@@ -10,7 +11,8 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 
 const PATHS = {
 	app: path.join(__dirname, 'app'),
-	build: path.join(__dirname, 'dist'),
+	build: path.join(__dirname, 'build'),
+	dist: path.join(__dirname, 'dist'),
 }
 
 const LAUNCH_COMMAND = process.env.npm_lifecycle_event
@@ -25,31 +27,28 @@ const productionPlugin = new webpack.DefinePlugin({
 })
 
 const base = {
-	output: {
-		path: PATHS.build,
-		filename: 'index_bundle.js',
-	},
 	module: {
 		loaders: [
 			{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-			{ test: /\.css$/, loader: 'style-loader!css-loader' },
 			{ test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
 		],
 	},
 }
 
 const developmentConfig = {
-	entry: [
-		'webpack-dev-server/client?http://localhost:8080',
-		'webpack/hot/only-dev-server',
-		'./app/index',
-	],
+	entry: {
+		main: './app/index.js',
+	},
+	output: {
+		path: PATHS.dist,
+		filename: 'bundle.js',
+	},
 	devtool: 'cheap-module-inline-source-map',
 	devServer: {
-		contentBase: PATHS.build,
+		contentBase: PATHS.dist,
 		hot: true,
 		inline: true,
-		quiet: true,
+		quiet: false,
 	},
 	plugins: [
 		HtmlWebpackPluginConfig,
@@ -58,13 +57,26 @@ const developmentConfig = {
 }
 
 const productionConfig = {
-	entry: [
-		'./app/index',
-	],
+	entry: {
+		main: './app/index.js',
+		vendor: ['react', 'react-dom', 'react-router'],
+	},
+	output: {
+		path: PATHS.build,
+		filename: '[chunkhash].[name].js',
+	},
 	devtool: 'cheap-module-source-map',
 	plugins: [
 		HtmlWebpackPluginConfig,
 		productionPlugin,
+		new webpack.optimize.CommonsChunkPlugin({
+			name: ['vendor', 'manifest'],
+		}),
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'disabled',
+			generateStatsFile: true,
+			statsFilename: 'stats.json',
+		}),
 		new webpack.optimize.UglifyJsPlugin({
 			compress: {
 				warnings: false,
