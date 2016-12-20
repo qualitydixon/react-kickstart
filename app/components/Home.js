a5d84a77cdc0a397f215080f42409b3d76aeab8c
Mike Dixon <qualitydixon@gmail.com>
2016-12-19 21:55:16 -0700

    Update Copy

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,7 @@ export default class Home extends Component {
 		return (
 			<div>
 				{'This is your home component, which is currently set as the IndexRoute in app/config/routes.js. It shows when no other routes are active.'}<br/>
-				{'This boilerplate uses React, React-Router, Babel, ESLint, Webpack, and LESS'}
+				{'This boilerplate uses React, React Router 4, Babel, ESLint, Webpack 2, and SASS'}
 			</div>
 		)
 	}
