96ed458d4262c97db74c19fe6b81ce3fa60dde23
Mike Dixon <qualitydixon@gmail.com>
2017-04-29 16:10:29 -0600

    Apply prettier formatting to Home

--- app/components/Home.js
+++ app/components/Home.js
@@ -4,8 +4,13 @@ export default class Home extends Component {
 	render () {
 		return (
 			<div>
-				{'This is your home component, which is currently set as the IndexRoute in app/config/routes.js. It shows when no other routes are active.'}<br/>
-				{'This boilerplate uses React, React Router 4, Babel, ESLint, Webpack 2, and SASS'}
+				{
+					'This is your home component, which is currently set as the IndexRoute in app/config/routes.js. It shows when no other routes are active.'
+				}
+				<br />
+				{
+					'This boilerplate uses React, React Router 4, Babel, ESLint, Webpack 2, and SASS'
+				}
 			</div>
 		)
 	}
