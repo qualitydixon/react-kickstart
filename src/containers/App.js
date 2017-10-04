441e924ba526fb3ff79e5081e0c664d3d8a94445
Mike Dixon <qualitydixon@gmail.com>
2017-10-03 18:01:56 -0600

    prettify App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,9 +28,15 @@ export default class App extends Component {
 						}
 					</h2>
 					<ul>
-						<li><Link to="/">{'Home'}</Link></li>
-						<li><Link to="/about">{'About'}</Link></li>
-						<li><Link to="/topics">{'Topics'}</Link></li>
+						<li>
+							<Link to="/">{'Home'}</Link>
+						</li>
+						<li>
+							<Link to="/about">{'About'}</Link>
+						</li>
+						<li>
+							<Link to="/topics">{'Topics'}</Link>
+						</li>
 					</ul>
 					<CSSTransitionGroup
 						transitionName="fade"
