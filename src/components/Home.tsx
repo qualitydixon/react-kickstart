5d86e871250c9102a680a05de5a59a4e2a13ec31
Mike Dixon <qualitydixon@gmail.com>
2017-10-12 09:10:59 -0600

    :construction: New Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,13 +4,7 @@ export default class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				{
-					'This is your home component, which is currently set as the IndexRoute in app/config/routes.js. It shows when no other routes are active.'
-				}
-				<br />
-				{
-					'This boilerplate uses React, React Router 4, Babel, ESLint, Webpack 2, and SASS'
-				}
+				<p>{'Welcome to React Kickstart!'}</p>
 			</div>
 		);
 	}
