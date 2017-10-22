5f2f8c5ecf9eba1aa26349170d3d530a6572ea98
Mike Dixon <qualitydixon@gmail.com>
2017-10-22 14:28:39 -0600

    :sparkles: New Home Page Design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ export default class Home extends React.Component {
 	render() {
 		return (
 			<div>
-				<p>{'Welcome to React Kickstart!'}</p>
+				<h2>{'Welcome to React Kickstart!'}</h2>
 			</div>
 		);
 	}
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,151 +1,67 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import { CSSTransitionGroup } from 'react-transition-group';
+import { Route, Switch, Link, withRouter } from 'react-router-dom';
 import Home from 'components/Home';
+import Header from 'components/Header';
 import styled from 'styles/styled-components';
 require('../styles/main.scss');
 
-interface AppState {
-	items: Array<string>;
-	newItem: string;
+interface Props {
+	location: {
+		key: string;
+	};
 }
-export default class App extends React.Component<undefined, AppState> {
-	constructor(props) {
-		super(props);
-		this.state = {
-			items: ['candle', 'reindeer', 'baseball', 'magazine'],
-			newItem: ''
-		};
-	}
 
-	handleChange(e) {
-		e.persist();
-		this.setState(() => ({ newItem: e.target.value }));
-	}
+class App extends React.Component<any, any> {
 	render() {
-		const { newItem, items } = this.state;
+		const { location } = this.props;
 		return (
-			<BrowserRouter>
-				<div>
-					<TitleWrapper>
-						<AppTitle>
-							<span>{'React'}</span>
-							<TitleDecorator>{'React'}</TitleDecorator>
-						</AppTitle>
-						<AppTitle>{'Kickstart'}</AppTitle>
-					</TitleWrapper>
-					<ul>
+			<div>
+				<Header />
+				<Navigation>
+					<NavList>
 						<li>
 							<Link to="/">{'Home'}</Link>
 						</li>
 						<li>
-							<Link to="/about">{'About'}</Link>
+							<Link to="/2">{'Other Route'}</Link>
 						</li>
 						<li>
-							<Link to="/topics">{'Topics'}</Link>
+							<Link to="/3">{'Another Route'}</Link>
 						</li>
-					</ul>
-					<CSSTransitionGroup
-						transitionName="fade"
-						transitionEnterTimeout={300}
-						transitionLeaveTimeout={300}
-					>
-						{items.map((item, idx) => <div key={idx}>{item}</div>)}
-					</CSSTransitionGroup>
-					<input
-						type="text"
-						placeholder="new item"
-						value={newItem}
-						className="input"
-						onChange={e => this.handleChange(e)}
-					/>
-					<button
-						onClick={() =>
-							this.setState(prevState => ({
-								items: prevState.items.concat([newItem]),
-								newItem: ''
-							}))}
-					>
-						submit
-					</button>
-					<Switch>
-						<Route exact path="/" component={Home} key="Home" />
-						<Route path="/about" component={About} />
-						<Route path="/topics" component={Topics} />
-						<Route component={NoMatch} />
-					</Switch>
-				</div>
-			</BrowserRouter>
+						<li>
+							<Link to="/asdf">{'404'}</Link>
+						</li>
+					</NavList>
+				</Navigation>
+				<Switch>
+					<Route exact path="/" component={Home} key="Home" />
+					<Route path="/2" component={OtherRoute} />
+					<Route path="/3" component={AnotherRoute} />
+					<Route component={NoMatch} />
+				</Switch>
+			</div>
 		);
 	}
 }
 
-interface AppTitleProps {
-	children: string;
-}
-const AppTitle = styled.h1`
-	text-transform: uppercase;
-	letter-spacing: 2px;
-	font-size: 15vw;
-	margin: 0;
-	font-weight: 700;
-	position: relative;
-	display: block;
-	background-color: #ffe2df;
-	z-index: 2;
-	&:before {
-		content: 'React';
-		display: none;
-		position: absolute;
-		top: -80%;
-		left: 0;
-		font-size: 15vw;
-		color: #464cdc;
-		overflow: hidden;
-		transform: translateY(50%);
-		z-index: -1;
-	}
-	> span {
-		&:first-child {
-			background-color: #ffe2df;
-			display: block;
-			line-height: 90%;
-		}
-	}
-`;
-
-const TitleWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	padding-top: 8em;
-`;
-
-const TitleDecorator = styled.span`
-	position: absolute;
-	top: -130%;
-	left: 0;
-	font-size: 15vw;
-	color: #464cdc;
-	overflow: hidden;
-	transform: translateY(50%);
-	z-index: -1;
-`;
-const About = () => (
+const Navigation = styled.nav`display: flex;`;
+const NavList = styled.ul`list-style-type: none;`;
+const OtherRoute = () => (
 	<div>
-		<h2>{'About'}</h2>
+		<h2>{'Other Route'}</h2>
 	</div>
 );
 
-const Topics = () => (
+const AnotherRoute = () => (
 	<div>
-		<h2>{'Topics'}</h2>
+		<h2>{'Another Route'}</h2>
 	</div>
 );
 
-const NoMatch = ({ location }) => (
+const NoMatch = () => (
 	<div>
-		<h2>{'Whoops'}</h2>
-		<p>{`Sorry but ${location.pathname} didn’t match any pages`}</p>
+		<h2>{'404 - Not Found'}</h2>
 	</div>
 );
+
+export default withRouter(App);
